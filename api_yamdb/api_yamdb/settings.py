b240cc3fddaa3cf11c01c839f45010efb92d55a6
Sergey Li <pincats@gmail.com>
2022-07-17 13:53:23 +0300

    env: Add apps to INSTALLED_APPS and change language to RU

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Users.apps.UsersConfig',
+    'Reviews.apps.ReviewsConfig',
+    'Api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -85,7 +88,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
