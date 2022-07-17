675d4a5f1f5362f98ffa672c9486495dd29d414b
Sergey Li <pincats@gmail.com>
2022-07-17 14:18:37 +0300

    feat: Add Category model

--- api_yamdb/Reviews/models.py
+++ api_yamdb/Reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('категория', max_length=256, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name[:15]
