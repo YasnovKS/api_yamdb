eb0e2b61384f4aa6e48332d54e711b70bcfa7e59
Sergey Li <pincats@gmail.com>
2022-07-17 14:45:26 +0300

    fix: Add Category max length 50 for slug

--- api_yamdb/Reviews/models.py
+++ api_yamdb/Reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField('имя категории', max_length=256)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name[:15]
