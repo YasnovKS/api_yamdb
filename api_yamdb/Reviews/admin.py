2efdade207e402cf1fcc1cbc4c5203444d156e26
Sergey Li <pincats@gmail.com>
2022-07-17 16:11:59 +0300

    feat: Add initial version of Title to admin panel

--- api_yamdb/Reviews/admin.py
+++ api_yamdb/Reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Genre
+from .models import Category, Genre, Title
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,7 +8,10 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
         'slug',
     )
-    search_fields = ('name', 'slug')
+    search_fields = (
+        'name',
+        'slug',
+    )
 
 
 class GenreAdmin(admin.ModelAdmin):
@@ -16,8 +19,32 @@ class GenreAdmin(admin.ModelAdmin):
         'name',
         'slug',
     )
-    search_fields = ('name', 'slug')
+    search_fields = (
+        'name',
+        'slug',
+    )
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'year',
+        'description',
+        'category',
+    )
+    search_fields = (
+        'name',
+        'description',
+    )
+    list_filter = (
+        'year',
+        'genre',
+        'category',
+    )
+    empty_value_display = '-пусто-'
+    list_editable = ('category',)
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
