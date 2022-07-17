ba47fed7fc9cecd428a584061fa445bceb34d7c1
Sergey Li <pincats@gmail.com>
2022-07-17 14:36:29 +0300

    feat: Add Category to admin panel

--- api_yamdb/Reviews/models.py
+++ api_yamdb/Reviews/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField('категория', max_length=256, unique=True)
+    name = models.CharField('имя категории', max_length=256, unique=True)
     slug = models.SlugField(unique=True)
 
     def __str__(self):
