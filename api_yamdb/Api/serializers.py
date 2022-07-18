830c689f21aee6c81d1a9e0863c29a29696b1da9
Sergey Li <pincats@gmail.com>
2022-07-19 01:10:51 +0300

    feat: Add serializers, urls, viewsets for Category, Genre, Title

---
+++ api_yamdb/Api/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+
+from Reviews.models import Category, Genre, GenreTitle, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'name',
+            'slug',
+        )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = (
+            'name',
+            'slug',
+        )
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True)
+    rating = serializers.SerializerMethodField()
+    category = CategorySerializer()
+
+    class Meta:
+        model = Genre
+        fields = (
+            'name',
+            'year',
+            'description',
+            'rating',
+            'genre',
+            'category',
+        )
+
+    def get_rating(self, obj):
+        # TODO: Add real calculation
+        return 0
+
+    def create(self, validated_data):
+        genres = validated_data.pop('genre')
+        title = Title.objects.create(**validated_data)
+        for genre in genres:
+            current_genre, status = Genre.objects.get_or_create(**genre)
+            GenreTitle.objects.create(achievement=current_genre, title=title)
+        return title
--- api_yamdb/Api/views.py
+++ api_yamdb/Api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .mixins import ListCreateDestroyViewSet
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+from Reviews.models import Category, Genre, Title
+
+
+class CategoryViewSet(ListCreateDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(ListCreateDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
