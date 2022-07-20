0f9440b77f8eaa5b714efe946920ec1d96efce50
Яснов Кирилл Сергеевич <YasnovKS@snc.local>
2022-07-20 16:29:55 +0500

    half progress

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,17 +1,31 @@
+import uuid
+
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
+from rest_framework.response import Response
 
 from reviews.models import Title, Review
 from api.serializers import (ReviewSerializer, CommentSerializer,
                              RegisterSerilizer)
 from django.core.mail import send_mail
 import uuid
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class RegisterViewSet(mixins.CreateModelMixin,
                       viewsets.GenericViewSet):
     confirmation_code = str(uuid.uuid4())
     serializer_class = RegisterSerilizer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK,
+                        headers=headers)
 
     def perform_create(self, serializer):
         serializer.save(confirmation_code=self.confirmation_code)
